a6340549652ba8eef9ac7790ab6cd228fec43ce4
Luis Fernando Martinez <luis@4040apps.com>
2021-04-17 21:42:32 -0500

    JWT sessionStorage

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -3,14 +3,11 @@ import { Article, ImgWrapper, Image } from './styles';
 import { Link } from '@reach/router';
 import { FavButton } from '../../components/FavButton'
 import { ToggleLikeMutation } from '../../container/ToggleLikeMutation';
-import { useLocalStorage } from '../../hooks/useLocalStorage';
 import { useNearScreen } from '../../hooks/useNearScreen';
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1504208434309-cb69f4fe52b0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60';
 
-export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE}) => {
-    const key = `like-${id}`;
-    const [liked, setLiked] = useLocalStorage(key, false);
+export const PhotoCard = ({id, likes = 0, liked, src = DEFAULT_IMAGE}) => {
     const [show, ref] = useNearScreen();
 
     return (
@@ -26,8 +23,7 @@ export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE}) => {
                 {
                     (toggleLike) => {
                         const handleFavClick = () => {
-                            !liked && toggleLike({ variables: {input: { id }} });
-                            setLiked(!liked);
+                            toggleLike({ variables: {input: { id }} });
                         }
                         return (<FavButton liked={liked} likes={likes} onClick={handleFavClick} />);
                     }
