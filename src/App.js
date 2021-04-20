bad4f87f98a46bb2d5674528a03cb26867d29697
Luis Fernando Martinez <luis@4040apps.com>
2021-04-19 22:01:06 -0500

    Not found page and redirects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { GlobalStyles } from './styles/GlobalStyles';
-import { Router } from '@reach/router'
-import Context from './Context';
+import { Redirect, Router } from '@reach/router';
+import { Context } from './Context';
 
 import { Logo } from './components/Logo';
 import { NavBar } from './components/NavBar';
@@ -10,36 +10,27 @@ import { Home } from './pages/Home';
 import { Detail } from './pages/Detail';
 import { Favs } from './pages/Favs';
 import { User } from './pages/User';
+import { NotFound } from './pages/NotFound';
 import { NotRegisteredUser } from './pages/NotRegisteredUser';
 
 export const App = () => {
+    const { isAuth } = useContext(Context);
     return (
         <div>
             <GlobalStyles />
             <Logo/>
             <Router>
+                <NotFound default/>
                 <Home path='/' />
                 <Home path='/pet/:categoryId' />
                 <Detail path='/detail/:detailId' />
+                { !isAuth && <NotRegisteredUser path='/login' />}
+                { !isAuth && <Redirect noThrow from='/favs' to='/login' />}
+                { !isAuth && <Redirect noThrow from='/user' to='/login' />}
+                { isAuth && <Redirect noThrow from='/login' to='/' />}
+                <Favs path='/favs' />
+                <User path='/user' />
             </Router>
-
-            <Context.Consumer>
-                {
-                    ({ isAuth }) => {
-                        return isAuth ?
-                         <Router>
-                            <Favs path='/favs' />
-                            <User path='/user' />
-                        </Router>
-                        :
-                        <Router>
-                            <NotRegisteredUser path='/favs' />
-                            <NotRegisteredUser path='/user' />
-                        </Router>
-                    } 
-                }                
-            </Context.Consumer>
-
             <NavBar />
         </div>
     );
---
+++ src/pages/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const NotFound = () => {
+    return (
+        <div>
+            La página no existe :c
+        </div>
+    );
+};
